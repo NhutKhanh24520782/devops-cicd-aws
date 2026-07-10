518b660de8a03dbc0f441476c7f9ec926fca757f
Lam Nhut Khanh <lamkhanhmt2020@gmail.com>
2026-07-10 15:52:33 +0700

    (Terraform)-Modify root main

--- terraform/main.tf
+++ terraform/main.tf
@@ -68,3 +68,9 @@ module "ecr" {
   project_name = var.project_name
   environment  = var.environment
 }
+module "s3" {
+  source = "./modules/s3"
+
+  project_name = var.project_name
+  environment  = var.environment
+}
